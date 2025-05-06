9c3c05bd87485e661f5bebe04015567b9712b04d
bennyhodl <ben@bitcoinbay.foundation>
2025-05-05 22:58:57 -0400

    nostr impl from ddk nostr transport

---
+++ src/dlc.rs
@@ -0,0 +1,57 @@
+use crate::{DlcDevKitDlcManager, Squawkbox};
+use tokio::sync::watch;
+
+use async_trait::async_trait;
+use ddk::{Oracle, Storage, Transport, nostr};
+use dlc::secp256k1_zkp::PublicKey as BitcoinPublicKey;
+use dlc_messages::Message;
+use std::sync::Arc;
+
+#[async_trait]
+impl Transport for Squawkbox {
+    fn name(&self) -> String {
+        "nostr".to_string()
+    }
+
+    fn public_key(&self) -> BitcoinPublicKey {
+        nostr::nostr_to_bitcoin_pubkey(&self.keys.public_key())
+    }
+
+    /// Get messages that have not been processed yet.
+    async fn start<S: Storage, O: Oracle>(
+        &self,
+        mut stop_signal: watch::Receiver<bool>,
+        manager: Arc<DlcDevKitDlcManager<S, O>>,
+    ) -> Result<(), anyhow::Error> {
+        let listen_handle = self.start(stop_signal.clone(), manager);
+
+        // Wait for either task to complete or stop signal
+        tokio::select! {
+            _ = stop_signal.changed() => Ok(()),
+            res = listen_handle => res?,
+        }
+    }
+    /// Send a message to a specific counterparty.
+    async fn send_message(&self, counterparty: BitcoinPublicKey, message: Message) {
+        let nostr_counterparty = nostr::bitcoin_to_nostr_pubkey(&counterparty);
+        tracing::info!(
+            bitcoin_pk = counterparty.to_string(),
+            nostr_pk = nostr_counterparty.to_string(),
+            "Sending nostr message."
+        );
+        let event =
+            nostr::messages::create_dlc_msg_event(nostr_counterparty, None, message, &self.keys)
+                .unwrap();
+        match self.client.send_event(&event).await {
+            Err(e) => tracing::error!(error = e.to_string(), "Failed to send nostr event."),
+            Ok(e) => tracing::info!(event_id = e.val.to_string(), "Sent DLC message event."),
+        }
+    }
+    /// Connect to a relay.
+    async fn connect_outbound(&self, _pubkey: BitcoinPublicKey, host: &str) {
+        match self.client.add_relay(host).await {
+            Ok(_) => tracing::info!(host, "Added relay."),
+            Err(e) => tracing::error!(host, error = e.to_string(), "Could not add relay."),
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,135 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod dlc;
+
+use std::sync::Arc;
+
+use bitcoin::Network;
+use bitcoin::bip32::Xpriv;
+use ddk::chain::EsploraClient;
+use ddk::nostr::messages::{create_dlc_msg_event, handle_dlc_msg_event};
+use ddk::wallet::DlcDevKitWallet;
+use ddk::{Oracle, Storage, Transport};
+use ddk_manager::{CachedContractSignerProvider, SimpleSigner, SystemTimeProvider};
+use nostr_sdk::{Client, RelayPoolNotification};
+use nostr_sdk::{Keys, Timestamp, Url, secp256k1::Secp256k1};
+use tokio::sync::watch;
+use tokio::task::JoinHandle;
+
+/// DlcDevKit type alias for the [ddk_manager::manager::Manager]
+pub type DlcDevKitDlcManager<S, O> = ddk_manager::manager::Manager<
+    Arc<DlcDevKitWallet>,
+    Arc<CachedContractSignerProvider<Arc<DlcDevKitWallet>, SimpleSigner>>,
+    Arc<EsploraClient>,
+    Arc<S>,
+    Arc<O>,
+    Arc<SystemTimeProvider>,
+    Arc<DlcDevKitWallet>,
+    SimpleSigner,
+>;
+
+pub struct Squawkbox {
+    pub keys: Keys,
+    pub relay_url: Url,
+    pub client: Client,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Squawkbox {
+    pub async fn new(
+        seed_bytes: &[u8; 32],
+        relay_host: &str,
+        network: Network,
+    ) -> anyhow::Result<Squawkbox> {
+        tracing::info!("Creating Nostr Dlc handler.");
+        let secp = Secp256k1::new();
+        let seed = Xpriv::new_master(network, seed_bytes)?;
+        let keys = Keys::new_with_ctx(&secp, seed.private_key.into());
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let relay_url = relay_host.parse()?;
+        let client = Client::new(keys.clone());
+        client.add_relay(&relay_url).await?;
+        client.connect().await;
+
+        Ok(Squawkbox {
+            keys,
+            relay_url,
+            client,
+        })
+    }
+
+    pub fn start<S: Storage, O: Oracle>(
+        &self,
+        mut stop_signal: watch::Receiver<bool>,
+        manager: Arc<DlcDevKitDlcManager<S, O>>,
+    ) -> JoinHandle<Result<(), anyhow::Error>> {
+        tracing::info!(
+            pubkey = self.keys.public_key().to_string(),
+            transport_public_key = self.public_key().to_string(),
+            "Starting Nostr DLC listener."
+        );
+        let nostr_client = self.client.clone();
+        let keys = self.keys.clone();
+        tokio::spawn(async move {
+            let since = Timestamp::now();
+            let msg_subscription =
+                ddk::nostr::messages::create_dlc_message_filter(since, keys.public_key());
+            nostr_client.subscribe(msg_subscription, None).await?;
+            tracing::info!(
+                "Listening for messages on {}",
+                keys.public_key().to_string()
+            );
+            let mut notifications = nostr_client.notifications();
+            loop {
+                tokio::select! {
+                    _ = stop_signal.changed() => {
+                        if *stop_signal.borrow() {
+                            tracing::warn!("Stopping nostr dlc message subscription.");
+                            nostr_client.disconnect().await;
+                            break;
+                        }
+                    },
+                    Ok(notification) = notifications.recv() => {
+                        if let RelayPoolNotification::Event {
+                            relay_url: _,
+                            subscription_id: _,
+                            event,
+                        } = notification {
+                            let (pubkey, message, event) = match handle_dlc_msg_event(
+                                &event,
+                                keys.secret_key(),
+                            ) {
+                                Ok(msg) => {
+                                    tracing::info!(pubkey=msg.0.to_string(), "Received DLC nostr message.");
+                                    (msg.0, msg.1, msg.2)
+                                },
+                                Err(_) => {
+                                    tracing::error!("Could not parse event {}", event.id);
+                                    continue;
+                                }
+                            };
+
+                            match manager.on_dlc_message(&message, pubkey).await {
+                                Ok(Some(msg)) => {
+                                    let event = create_dlc_msg_event(
+                                        event.pubkey,
+                                        Some(event.id),
+                                        msg,
+                                        &keys,
+                                    )
+                                    .expect("no message");
+                                    nostr_client
+                                        .send_event(&event)
+                                        .await
+                                        .expect("Break out into functions.");
+                                }
+                                Ok(None) => (),
+                                Err(_) => {
+                                    // handle the error case and send
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            Ok::<_, anyhow::Error>(())
+        })
     }
 }
